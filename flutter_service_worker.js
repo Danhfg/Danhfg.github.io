5b13642b84f2680212b07d3608d8e980f96ad2e9
Daniel Henrique Ferreira Gomes <danielhenriquefg@gmail.com>
2022-01-14 18:05:15 -0300

    fix search

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -28,10 +28,10 @@ const RESOURCES = {
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"index.html": "9025cf875a84c2e2f91eecfbde825ca5",
-"/": "9025cf875a84c2e2f91eecfbde825ca5",
+"index.html": "d0703302601520430565046bf7a095e8",
+"/": "d0703302601520430565046bf7a095e8",
 "main.dart.js": "3674258caef0153e883fbb2dd8072455",
-"manifest.json": "6a9666d47c7f2b3c95f892e1786d47b1",
+"manifest.json": "6164b7af5a1e70c6e5e8c85584409527",
 "version.json": "8ed7f85094b2da843cad4e017ab1d5b1"
 };
 
